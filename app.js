987b0fe264c7f09f4f105dd9d5f1b86a4cb0bb3c
rameem2003 <rameem2019@gmail.com>
2022-10-08 16:10:16 +0600

    bug

--- app.js
+++ app.js
@@ -5,18 +5,18 @@ const secondHand = document.getElementById('[data-second-hand]')
 
 function setClock(){
     const curDate = new Date();
-    const secondTime = curDate.getSeconds() / 60;
-    const minuteTime = (secondTime + curDate.getMinutes()) / 60;
-    const hourTime = (minuteTime + curDate.getHours()) / 12;
+    const secondRatio = curDate.getSeconds() / 60;
+    const minuteRatio = (secondRatio + curDate.getMinutes()) / 60;
+    const hourRatio = (minuteRatio + curDate.getHours()) / 12;
 
-    setRotation(secondHand, secondTime);
-    setRotation(minuteHand, minuteTime)
-    setRotation(hourHand, hourTime);
+    setRotation(hourHand, hourRatio);
+    setRotation(secondHand, secondRatio);
+    setRotation(minuteHand, minuteRatio);
 }
 
 
-function setRotation(document, rotationRatio){
-    document.style.setProperty("--rotation", rotationRatio * 360);
+function setRotation(element, rotationRatio){
+    element.style.setProperty('--rotation', rotationRatio * 360);
 }
 
 setClock()
